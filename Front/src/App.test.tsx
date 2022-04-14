8c0dcb9f75751ef2bf599b7fff74b6b5e5c73275
enzotoyos <enzo.toyos@gmail.com>
2022-04-14 14:30:29 +0200

    création test fonctionneles fronts

--- Front/src/App.test.tsx
+++ Front/src/App.test.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import App from "./App";
+import Cart from "../src/components/Cart";
+import Home from "../src/components/Home";
+import Product from "../src/components/Product";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: any;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+test("test Cart component", () => {
+  act(() => {
+    ReactDOM.render(
+      <Product data={{ name: "Figurine de Rick Sanchez", quantity: 10 }} />,
+      container
+    );
+  });
+  const title = screen.getByText(/Figurine de Rick Sanchez/i);
+  expect(title).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
+
+  const quantity = screen.getByText(/10/i);
+  expect(quantity).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
+});
+
+test("test Cart component", () => {
+  act(() => {
+    ReactDOM.render(<Cart setRoute={() => console.log("")} />, container);
+  });
+  const title = screen.getByText(/Figurine de/i);
+  expect(title).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
+
+  const quantity = screen.getByText(/Quantitée/i);
+  expect(quantity).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
+});
+
+test("test Home component", () => {
+  act(() => {
+    ReactDOM.render(<Home setRoute={() => console.log("")} />, container);
+  });
+  const title = screen.getByText(/Aller sur panier/i);
+  expect(title).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
+
+  const quantity = screen.getByText(/Quantitée/i);
+  expect(quantity).toBeInTheDocument();
+  expect(container.getElementsByClassName("touch").length);
 });
