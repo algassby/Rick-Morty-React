7ea8c81e8fd03516c87ffb217d961e3266432dff
enzotoyos <enzo.toyos@gmail.com>
2022-04-14 11:42:23 +0200

    modifs

--- Front/cypress/integration/1-UITests/homeTests.js
+++ Front/cypress/integration/1-UITests/homeTests.js
@@ -3,11 +3,15 @@
 describe("test ajout panier", () => {
   it("ajout d'une carte Rick Sanchez", () => {
     cy.visit("http://localhost:3000");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.contains("Figurine de Rick Sanchez").click();
+    cy.wait(3000);
     cy.contains("Ajouter au panier").click();
+    cy.wait(3000);
     cy.contains("Retour").click();
+    cy.wait(3000);
     cy.contains("Aller sur panier").click();
+    cy.wait(3000);
     cy.contains("Retour").click();
   });
 });
@@ -17,7 +21,7 @@ describe("test ajout panier", () => {
 describe("test supression panier", () => {
   it("supresssion d'une carte Rick Sanchez", () => {
     cy.visit("http://localhost:3000");
-    cy.wait(2000);
+    cy.wait(3000);
     cy.contains("Aller sur panier").click();
     cy.wait(5000);
     cy.contains("Supprimer du panier").click();
