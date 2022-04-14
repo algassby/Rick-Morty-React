047da1a20c87688a394aed6b30782e07ca936855
enzotoyos <enzo.toyos@gmail.com>
2022-04-14 11:02:49 +0200

    modif cypress

--- Front/cypress/integration/1-UITests/homeTests.js
+++ Front/cypress/integration/1-UITests/homeTests.js
@@ -1,10 +1,25 @@
-describe("Calculator", () => {
-  it("Test addition", () => {
+///////////////////// Ajout de l'item //////////////////////////////
+
+describe("test ajout panier", () => {
+  it("ajout d'une carte Rick Sanchez", () => {
     cy.visit("http://localhost:3000");
-    cy.contains("1").click();
-    cy.contains("+").click();
-    cy.contains("2").click();
-    cy.contains("=").click();
-    cy.get(".result").contains("3");
+    cy.wait(2000);
+    cy.contains("Figurine de Rick Sanchez").click();
+    cy.contains("Ajouter au panier").click();
+    cy.contains("Retour").click();
+    cy.contains("Aller sur panier").click();
+    cy.contains("Retour").click();
+  });
+});
+
+///////////////////// Suppression de l'item //////////////////////////////
+
+describe("test supression panier", () => {
+  it("supresssion d'une carte Rick Sanchez", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(2000);
+    cy.contains("Aller sur panier").click();
+    cy.wait(5000);
+    cy.contains("Supprimer du panier").click();
   });
 });
